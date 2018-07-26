75cbdfcfe5525e860189a9f223a1b565cb0dd196
VoThienTrung <vothientrung90@gmail.com>
2018-07-26 15:05:32 +0700

    Init checkListTableView

--- FCS/FCS/ViewController.swift
+++ FCS/FCS/ViewController.swift
@@ -8,18 +8,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ChecklistTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var questionTextView: UITextView!
+    
+    @IBOutlet weak var firstChoiceButton: UIButton!
+    
+    @IBOutlet weak var secondChoiceButton: UIButton!
+    
+    @IBOutlet weak var thirdChoiceButton: UIButton!
+    
+    @IBOutlet weak var reviewTextView: UITextView!
+    
+    @IBOutlet weak var imageButton: UIButton!
+    
+}
 
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customTableViewCell", for: indexPath)
+            as! ChecklistTableViewCell
+        
+        cell.questionTextView?.text = "This is a question"
+        cell.reviewTextView?.text = "User's review"
+        
+        return cell
+    }
+    
+    
+    @IBOutlet weak var completedButton: UIButton!
+    
+    @IBOutlet weak var checklistTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.checklistTableView.delegate = self
+        self.checklistTableView.dataSource = self
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
+
+
 
