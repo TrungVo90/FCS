734176bfc8d079359a423996e8b0c9b42e09998b
Unknown <vothientrung90@gmail.com>
2018-07-30 17:58:00 +0700

    Auto stash before merge of "develop" and "origin/develop"

--- FCS/FCS/PreviewChecklistsViewController.swift
+++ FCS/FCS/PreviewChecklistsViewController.swift
@@ -9,7 +9,23 @@
 import Foundation
 import UIKit
 
-class PreviewChecklistsViewController: UIViewController {
+class PreviewChecklistTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var questionTextView: UITextView!
+    
+    @IBOutlet weak var firstChoiceButton: UIButton!
+    
+    @IBOutlet weak var secondChoiceButton: UIButton!
+    
+    @IBOutlet weak var thirdChoiceButton: UIButton!
+    
+    @IBOutlet weak var reviewTextView: UITextView!
+    
+    @IBOutlet weak var imageButton: UIButton!
+}
+
+class PreviewChecklistsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var backButon: UIButton!
     
     @IBOutlet weak var userInfoButton: UIButton!
@@ -17,6 +33,13 @@ class PreviewChecklistsViewController: UIViewController {
     @IBOutlet weak var submitButton: UIButton!
     
     @IBOutlet weak var previewChecklistTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.previewChecklistTableView.delegate = self
+        self.previewChecklistTableView.dataSource = self
+    }
     
     @IBAction func backButtonOnClick(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
@@ -30,5 +53,22 @@ class PreviewChecklistsViewController: UIViewController {
     
     @IBAction func submitButtonOnClick(_ sender: Any) {
         
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 235
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "previewChecklistTableViewCell", for: indexPath)
+            as! PreviewChecklistTableViewCell
+        
+        cell.questionTextView?.text = "This is a question"
+        cell.reviewTextView?.text = "User's review"
+        return cell
     }
 }
