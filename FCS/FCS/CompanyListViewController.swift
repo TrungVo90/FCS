25d3c7b34d9c3577912942988efbad7c2f2fc752
Unknown <vothientrung90@gmail.com>
2018-08-24 11:46:49 +0700

    [TrungVo]-Implement search bar function in two files: Store & Company

--- FCS/FCS/CompanyListViewController.swift
+++ FCS/FCS/CompanyListViewController.swift
@@ -26,17 +26,22 @@ class CompanyListTableViewCell: UITableViewCell {
     }
 }
 
-class CompanyListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class CompanyListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
     @IBOutlet weak var companyTableView: UITableView!
     
     @IBOutlet weak var userInfoButton: UIButton!
     
+    @IBOutlet weak var companySearchBar: UISearchBar!
     fileprivate var _companies = [Companies]()
+    var filtered: [Companies] = []
+    var searchActive : Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.companyTableView.delegate = self
         self.companyTableView.dataSource = self
+        self.companySearchBar.delegate = self
         
         /// Load list of company on UI
         if let unarchivedObject = UserDefaults.standard.object(forKey: "companies") as? Data {
@@ -56,18 +61,54 @@ class CompanyListViewController: UIViewController, UITableViewDelegate, UITableV
         // Dispose of any resources that can be recreated.
     }
     
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        searchActive = true;
+    }
+    
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        searchActive = false;
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchActive = false;
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchActive = false;
+    }
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 70
     }
     
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        filtered = self._companies.filter({ (company) -> Bool in
+            let tmp: String = company.name
+            let range = tmp.range(of: searchText, options: String.CompareOptions.caseInsensitive)
+            return range?.isEmpty == false
+        })
+        if(filtered.count == 0){
+            searchActive = false
+        } else {
+            searchActive = true
+        }
+        self.companyTableView.reloadData()
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if(searchActive) {
+            return self.filtered.count
+        }
         return self._companies.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "companyListTableViewCell", for: indexPath)
             as! CompanyListTableViewCell
-        let company = self._companies[indexPath.row]
+        var company = self._companies[indexPath.row]
+        if(searchActive){
+            company = filtered[indexPath.row]
+        }
         cell.companyNameLabel.text = company.name
         cell.companyDescription.text = company.address
         
--- FCS/FCS/StoreListViewCompany.swift
+++ FCS/FCS/StoreListViewCompany.swift
@@ -24,11 +24,16 @@ class StoreListTableViewCell: UITableViewCell {
     }
 }
 
-class StoreListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class StoreListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
     @IBOutlet weak var storeTableView: UITableView!
     var company_id: Int64 = 0
     fileprivate var _branchs = [Branchs]()
     
+    @IBOutlet weak var storeSearchBar: UISearchBar!
+    
+    var filtered: [Branchs] = []
+    
+    var searchActive : Bool = false
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         /// Load list of company on UI
@@ -39,11 +44,14 @@ class StoreListViewController: UIViewController, UITableViewDelegate, UITableVie
         /// Filter store based on company id
         self._branchs = self.getListOfBranchsBasedOnCompanyId(companyId: self.company_id)
     }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.storeTableView.delegate = self
         self.storeTableView.dataSource = self
+        self.storeSearchBar.delegate = self
     }
     
     private func getListOfBranchsBasedOnCompanyId(companyId: Int64) -> [Branchs] {
@@ -61,19 +69,55 @@ class StoreListViewController: UIViewController, UITableViewDelegate, UITableVie
         // Dispose of any resources that can be recreated.
     }
     
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        searchActive = true;
+    }
+    
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        searchActive = false;
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchActive = false;
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchActive = false;
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        filtered = _branchs.filter({ (branch) -> Bool in
+            let tmp: String = branch.name
+            let range = tmp.range(of: searchText, options: String.CompareOptions.caseInsensitive)
+            return range?.isEmpty == false
+        })
+        if(filtered.count == 0){
+            searchActive = false
+        } else {
+            searchActive = true
+        }
+        self.storeTableView.reloadData()
+    }
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 70
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if(searchActive) {
+            return self.filtered.count
+        }
         return self._branchs.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "storeListTableViewCell", for: indexPath)
             as! StoreListTableViewCell
-        let branch = self._branchs[indexPath.row]
-       cell.storeNameLabel.text = branch.name
+        var branch = self._branchs[indexPath.row]
+        if(searchActive){
+            branch = filtered[indexPath.row]
+        }
+        cell.storeNameLabel.text = branch.name
         cell.storeDescription.text = "Address: " + branch.address + " - Phone: " + branch.phone + "\n Email: " + branch.email
         return cell
     }
